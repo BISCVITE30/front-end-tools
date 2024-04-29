b7a1856d5f340aa82a5785a4889b0a89ca807648
BISCVITE30 <gonnki2004@gmail.com>
2024-04-29 17:10:31 +0200

    commit files

---
+++ lesson6-webpack-loaders/task1/src/index.js
@@ -0,0 +1,9 @@
+import renderProfile from './profile/renderProfile.js';
+import './index.css';
+
+const profileData = {
+    name: 'Tom',
+    location: 'The Wordl',
+}
+
+renderProfile(profileData);
